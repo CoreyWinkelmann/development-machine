714b7eb50d9b14f7e2e2f115d6ae12b20166e53e
Corey Winkelmann <cawinkelmann@gmail.com>
2021-08-31 21:53:25 -0600

    Install asdf

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@
 mv ~/.vimrc ~/vimrc_backup 2> /dev/null
 mv ~/.gitconfig ~/gitconfig_backup 2> /dev/null
 
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/gitconfig ~/.gitconfig
+THISDIR=$(pwd)
+
+ln -s $THISDIR/vimrc ~/.vimrc
+ln -s $THISDIR/gitconfig ~/.gitconfig
 
 # Setup Vim Plugins
 vim +PlugInstall +qall
@@ -14,10 +16,17 @@ ZSHRC=~/.zshrc
 if [ ! -f "$ZSHRC" ]; then
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	mv ~/.zshrc ~/zshrc_backup 2> /dev/null
-	ln -s $(pwd)/zshrc ~/.zshrc
+	ln -s $THISDIR/zshrc ~/.zshrc
 fi
 
 ASDF=~/.asdf
-if [ ! -f "$ASDF" ]; then
+if [ -f "$ASDF" ]; then
+	cd ~/.asdf
+	git pull origin
+else
 	git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+
+	asdf plugin-add golang https://github.com/kennyp/asdf-golang.git
 fi
+
+source ~/.zshrc
