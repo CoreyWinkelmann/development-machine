f842bc769518b5c5fd90505d8c2805005e436bda
Corey Winkelmann <cawinkelmann@gmail.com>
2021-09-07 11:59:47 -0600

    Updates go setup to allow a passed version otherwise defaults latest

--- go/setup.sh
+++ go/setup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-asdf install golang latest
-asdf global golang latest
+GO_VERSION="${1:-latest}"
+
+asdf install golang $GO_VERSION
+asdf global golang $GO_VERSION
 
 go install github.com/go-delve/delve/cmd/dlv@latest
 go install github.com/jstemmer/gotags@latest
-go install golang.org/x/tools/cmd/guru@latest
+go install golang.org/x/tools/cmd/...@latest
 # go install github.com/golangci/golangci-lint@latest
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.42.0
 
